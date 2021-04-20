5abfd1171d7a47ea08fd3b7e06fdc2756086d364
Jonathan Yeh <jmyeh51@gmail.com>
2021-04-20 11:37:52 -0700

    got rid of force updates, React part still needs some work

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -12,17 +12,25 @@ export default function Sudoku() {
                                         [0, 0, 0, 0, 0, 0, 0, 0, 3],
                                         [0, 0, 5, 7, 0, 0, 0, 6, 8],
                                         [0, 7, 8, 1, 0, 3, 2, 5, 0]])
-  const update = useForceUpdate()
+  let originalBoard = [[0, 1, 6, 3, 0, 8, 4, 2, 0],
+                        [8, 4, 0, 0, 0, 7, 3, 0, 0],
+                        [3, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 6, 0, 9, 4, 0, 8, 0, 2],
+                        [0, 8, 1, 0, 3, 0, 7, 9, 0],
+                        [9, 0, 3, 0, 7, 6, 0, 4, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 3],
+                        [0, 0, 5, 7, 0, 0, 0, 6, 8],
+                        [0, 7, 8, 1, 0, 3, 2, 5, 0]]
+  // const [originalBoard, setOriginalBoard] = useState(puzzle)
   const classes = useStyles()
   const [coordinates, setCoordinates] = useState([-1,-1])
-  const [sudoku, setSudoku] = useState(null)
   const [rowError, setRowError] = useState(false)
   const [colError, setColError] = useState(false)
   const [boxError, setBoxError] = useState(false)
   const [solving, setSolving] = useState(false)
   const [numberMap, setNumberMap] = useState(null)
-  const [puzzleStates, setPuzzleStates] = useState([])
-  const [stepState, setStepState] = useState(null)
+  const [boardState, setBoardState] = useState(0)
+  const [statesArray, setStatesArray] = useState([])
 
   class NumberTuple {
     constructor(row, col, value) {
@@ -32,7 +40,7 @@ export default function Sudoku() {
     }
   }
 
-  function sleep(milliseconds) {
+  const sleep = (milliseconds) => {
     const date = Date.now()
     let currentDate = null
     do {
@@ -102,12 +110,11 @@ export default function Sudoku() {
     return true
   }
 
-  function checkConstraints (board, row, col, value) {
+  const checkConstraints = (board, row, col, value) => {
     setCoordinates([row, col])
     const rc = rowConstraint(board, row, value)
     const cc = colConstraint(board, col, value)
     const bc = boxConstraint(board, row, col, value)
-    update()
     // No errors
     if (rc && cc && bc) {
           return true
@@ -116,12 +123,11 @@ export default function Sudoku() {
     return false
   }
 
-  function getNextEmptySpot (board) {
+  const getNextEmptySpot = (board) => {
     for (let row = 0; row < board.length; row++) {
       for (let col = 0; col < board[row].length; col++) {
         if (board[row][col] === 0) {
           setCoordinates([row, col])
-          update()
           return [row, col]
         }
       }
@@ -129,122 +135,113 @@ export default function Sudoku() {
     return [-1, -1]
   }
 
-  useEffect(() => {
-    let puzzleBoard = puzzle
-    let coords = [-1, -1]
-
-    const handleClick = (row, col) => {
-      let newPuzzle = puzzle
-      let val = newPuzzle[row][col]
-      if (val === 9) {
-        newPuzzle[row][col] = 0
-      } else {
-        newPuzzle[row][col]++
-      }
-      setPuzzle(newPuzzle)
-      setCoordinates([row, col])
-      console.log(puzzle)
-      update()
+  const handleClick = (row, col) => {
+    let newPuzzle = puzzle
+    let val = puzzle[row][col]
+    if (val === 9) {
+      puzzle[row][col] = 0
+    } else {
+      puzzle[row][col]++
     }
-
-    function solve(board) {
-      coords = getNextEmptySpot(board)
-      let row = coords[0]
-      let col = coords[1]
-
-      if (row === -1) {
-        setSolving(false)
-        return
-      }
-
-      console.log(board)
-      sleep(5)
-
-      for (let value = 1; value <= 9; value++) {
-        if (checkConstraints(board, row, col, value)) {
-          board[row][col] = value
-          setPuzzle(board)
-          let copy = puzzleStates
-          copy.push(board)
-          setPuzzleStates(copy)
-          setStepState(puzzleStates.length)
-          solve(puzzle)
-        }
-      }
-      if (getNextEmptySpot(board)[0] !== -1) {
-        board[row][col] = 0
-      }
-      return
-    }
-
-    const solveLoop = () => {
-      solve(puzzle)
-      update()
-    }
-
-    const getNumbers = () => {
-      let nums = []
-      for (let row = 0; row < puzzle.length; row++) {
-        for (let col = 0; col < puzzle[row].length; col++) {
-          nums.push(new NumberTuple(row, col, puzzle[row][col]))
-        }
-      }
-
-      const numMap = nums.map((num) =>
-        <SmallBox puzzle={puzzleBoard} coords={coords} value={num.value} row={num.row} col={num.col} handleClick={handleClick} />
-      )
-
-      setNumberMap(numMap)
-    }
-
-    getNumbers()
-
-    setSudoku({
-      getNumbers,
-      handleClick,
-      solveLoop,
-    })
-  }, [coordinates, setCoordinates])
-
-  const solv = () => sudoku.solveLoop()
-  const getNums = () => sudoku.getNumbers()
-
-  const updatePuzzle = (num) => {
-    const val = stepState + num
-    if (val > puzzleStates.length) {
-      return
-    }
-    if (val < 0) {
-      return
-    }
-    setStepState(val)
-    setPuzzle(puzzleStates[stepState])
-    getNums()
-    update()
-    console.log("PUZZLE", puzzleStates)
-    console.log(num)
-    console.log(stepState)
+    // setPuzzle(puzzle)
+    setCoordinates([row, col])
     console.log(puzzle)
   }
+
+  let stateArray = []
+
+  function getSolution (board) {
+    let coords = getNextEmptySpot(board)
+    let row = coords[0]
+    let col = coords[1]
+
+    // Base Case, no empty spots so we return the completeted board
+    if (row === -1) {
+      setStatesArray(stateArray)
+      setSolving(true)
+      return board
+    }
+
+    // Try every value that fits constraints
+    for (let value = 1; value <= 9; value++) {
+      if (checkConstraints(board, row, col, value)) {
+        board[row][col] = value
+        stateArray[stateArray.length] = [row, col, value]
+        getSolution(board)
+      }
+    }
+
+    // Unsolvable
+    if (getNextEmptySpot(board)[0] !== -1) {
+      board[row][col] = 0
+    }
+    return board
+  }
+
+  function solve () {
+    getSolution(puzzle)
+  }
+
+  const getNumbers = () => {
+    let nums = []
+    for (let row = 0; row < puzzle.length; row++) {
+      for (let col = 0; col < puzzle[row].length; col++) {
+        nums.push(new NumberTuple(row, col, puzzle[row][col]))
+      }
+    }
+
+    const numMap = nums.map((num) =>
+      <SmallBox puzzle={puzzle} coords={coordinates} value={num.value} row={num.row} col={num.col} handleClick={handleClick} />
+    )
+
+    setNumberMap(numMap)
+  }
+
+  useEffect(() => {
+    if (solving) {
+      let copy = originalBoard
+      if (boardState === statesArray.length) {
+        setSolving(!solving)
+        return
+      }
+      let row = statesArray[boardState][0]
+      let col = statesArray[boardState][1]
+      let value = statesArray[boardState][2]
+      copy[row][col] = value
+      originalBoard = copy
+      setCoordinates([row, col])
+      sleep(5)
+      setBoardState(boardState + 1)
+    }
+
+    // populate boxes
+    getNumbers()
+  }, [coordinates, boardState, solving])
 
   return (
     <div className={classes.wrapper}>
       <div className={classes.box}>
         <>{numberMap}</>
       </div>
-      <button disabled={solving} onClick={solv}>solve</button>
-      <button onClick={() => updatePuzzle(-10)}>-10</button>
-      <button onClick={() => updatePuzzle(-1)}>-1</button>
-      <button onClick={() => updatePuzzle(+1)}>+1</button>
-      <button onClick={() => updatePuzzle(+10)}>+10</button>
+      <button disabled={solving} onClick={solve}>solve</button>
     </div>
   )
 }
 
 const SmallBox = (props) => {
+  // console.log(props.coords[0], props.coords[1], props.row, props.col)
   const classes = useStyles()
+  function getColor() {
+    const row = props.coords[0]
+    const col = props.coords[1]
+    if (props.row === row && props.col === col) {
+      return '#FF0000'
+    } else {
+      return "#FFFFFF"
+    }
+  }
   return(
-    <div className={classes.smallBox} onClick={() => {props.handleClick(props.row, props.col)}}>
+    <div className={classes.smallBox} style={{background: `${getColor()}`}} onClick={() => {props.handleClick(props.row, props.col)}}>
       {props.puzzle[props.row][props.col]}
     </div>
   )
@@ -276,16 +273,9 @@ const useStyles = makeStyles(() => ({
     alignItems: 'center',
     width: '62px',
     height: '62px',
-    background: '#FFFFFF',
     border: '1px solid black',
     '&:hover': {
       background: '#00DDFF'
     }
   }
 }))
-
-function useForceUpdate() {
-  // eslint-disable-next-line
-  const [value, setValue] = useState(0) // integer state
-  return () => setValue(value => value + 1) // update the state to force render
-}
