303ac030adbd5348d5765627abdc2faa13cc3684
Jonathan Yeh <jmyeh51@gmail.com>
2021-04-20 20:41:37 -0700

    Visualizer working on first puzzle test

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -1,36 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 
 export default function Sudoku() {
-  const [puzzle, setPuzzle] = useState([[0, 1, 6, 3, 0, 8, 4, 2, 0],
-                                        [8, 4, 0, 0, 0, 7, 3, 0, 0],
-                                        [3, 0, 0, 0, 0, 0, 0, 0, 0],
-                                        [0, 6, 0, 9, 4, 0, 8, 0, 2],
-                                        [0, 8, 1, 0, 3, 0, 7, 9, 0],
-                                        [9, 0, 3, 0, 7, 6, 0, 4, 0],
-                                        [0, 0, 0, 0, 0, 0, 0, 0, 3],
-                                        [0, 0, 5, 7, 0, 0, 0, 6, 8],
-                                        [0, 7, 8, 1, 0, 3, 2, 5, 0]])
-  let originalBoard = [[0, 1, 6, 3, 0, 8, 4, 2, 0],
-                        [8, 4, 0, 0, 0, 7, 3, 0, 0],
-                        [3, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 6, 0, 9, 4, 0, 8, 0, 2],
-                        [0, 8, 1, 0, 3, 0, 7, 9, 0],
-                        [9, 0, 3, 0, 7, 6, 0, 4, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 3],
-                        [0, 0, 5, 7, 0, 0, 0, 6, 8],
-                        [0, 7, 8, 1, 0, 3, 2, 5, 0]]
-  // const [originalBoard, setOriginalBoard] = useState(puzzle)
+  const originalBoard = [[0, 1, 6, 3, 0, 8, 4, 2, 0],
+                          [8, 4, 0, 0, 0, 7, 3, 0, 0],
+                          [3, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 6, 0, 9, 4, 0, 8, 0, 2],
+                          [0, 8, 1, 0, 3, 0, 7, 9, 0],
+                          [9, 0, 3, 0, 7, 6, 0, 4, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 3],
+                          [0, 0, 5, 7, 0, 0, 0, 6, 8],
+                          [0, 7, 8, 1, 0, 3, 2, 5, 0]]
+  const [puzzle, setPuzzle] = useState(originalBoard)
+
   const classes = useStyles()
-  const [coordinates, setCoordinates] = useState([-1,-1])
-  const [rowError, setRowError] = useState(false)
-  const [colError, setColError] = useState(false)
-  const [boxError, setBoxError] = useState(false)
+  const coordinates = useRef([-1,-1])
   const [solving, setSolving] = useState(false)
   const [numberMap, setNumberMap] = useState(null)
   const [boardState, setBoardState] = useState(0)
   const [statesArray, setStatesArray] = useState([])
+  const [nodesList, setNodesList] = useState([])
 
   class NumberTuple {
     constructor(row, col, value) {
@@ -48,17 +38,16 @@ export default function Sudoku() {
     } while (currentDate - date < milliseconds)
   }
 
+  // Constraints when adding a value
   const rowConstraint = (board, row, value) => {
     for (let col = 0; col < board[row].length; col++) {
       if (board[row][col] === 0) {
         continue
       }
       if (board[row][col] === value) {
-        setRowError(true)
         return false
       }
     }
-    setRowError(false)
     return true
   }
 
@@ -68,11 +57,9 @@ export default function Sudoku() {
         continue
       }
       if (board[row][col] === value) {
-        setColError(true)
         return false
       }
     }
-    setColError(false)
     return true
   }
 
@@ -101,20 +88,97 @@ export default function Sudoku() {
           continue
         }
         if (board[posRow + r][posCol + c] === value) {
-          setBoxError(true)
           return false
         }
       }
     }
-    setBoxError(false)
     return true
   }
 
   const checkConstraints = (board, row, col, value) => {
-    setCoordinates([row, col])
     const rc = rowConstraint(board, row, value)
     const cc = colConstraint(board, col, value)
     const bc = boxConstraint(board, row, col, value)
+    console.log(row, col, rc, cc, bc)
+    // No errors
+    if (rc && cc && bc) {
+          return true
+    }
+    // At least one error
+    return false
+  }
+
+  // Constraints for the visualizer
+  const rowVisualizerConstraint = (board, row) => {
+    let visited = new Set()
+    for (let col = 0; col < board[row].length; col++) {
+      let value = board[row][col]
+      if (value === 0) {
+        continue
+      }
+      if (visited.has(value)) {
+        return false
+      }
+      visited.add(value)
+    }
+    return true
+  }
+
+  const colVisualizerConstraint = (board, col) => {
+    let visited = new Set()
+    for (let row = 0; row < board.length; row++) {
+      let value = board[row][col]
+      if (value === 0) {
+        continue
+      }
+      if (visited.has(value)) {
+        return false
+      }
+      visited.add(value)
+    }
+    return true
+  }
+
+  const boxVisualizerConstraint = (board, row, col) => {
+    let visited = new Set()
+    let posRow = 0
+    let posCol = 0
+    if (row <= 2) {
+      posRow = 1
+    } else if (row >= 6) {
+      posRow = 7
+    } else {
+      posRow = 4
+    }
+
+    if (col <= 2) {
+      posCol = 1
+    } else if (col >= 6) {
+      posCol = 7
+    } else {
+      posCol = 4
+    }
+
+    for (let r = -1; r <= 1; r++) {
+      for (let c = -1; c <= 1; c++) {
+        let value = board[posRow + r][posCol + c]
+        if (value === 0) {
+          continue
+        }
+        if (visited.has(value)) {
+          return false
+        }
+        visited.add(value)
+      }
+    }
+    return true
+  }
+
+  const checkVisualizerConstraints = (board, row, col) => {
+    const rc = rowVisualizerConstraint(board, row)
+    const cc = colVisualizerConstraint(board, col)
+    const bc = boxVisualizerConstraint(board, row, col)
+    console.log(row, col, rc, cc, bc)
     // No errors
     if (rc && cc && bc) {
           return true
@@ -127,7 +191,7 @@ export default function Sudoku() {
     for (let row = 0; row < board.length; row++) {
       for (let col = 0; col < board[row].length; col++) {
         if (board[row][col] === 0) {
-          setCoordinates([row, col])
+          coordinates.current = [row, col]
           return [row, col]
         }
       }
@@ -136,20 +200,16 @@ export default function Sudoku() {
   }
 
   const handleClick = (row, col) => {
-    let newPuzzle = puzzle
     let val = puzzle[row][col]
     if (val === 9) {
       puzzle[row][col] = 0
     } else {
       puzzle[row][col]++
     }
-    // setPuzzle(puzzle)
-    setCoordinates([row, col])
-    console.log(puzzle)
+    coordinates.current = [row, col]
   }
 
   let stateArray = []
-
   function getSolution (board) {
     let coords = getNextEmptySpot(board)
     let row = coords[0]
@@ -159,7 +219,7 @@ export default function Sudoku() {
     if (row === -1) {
       setStatesArray(stateArray)
       setSolving(true)
-      return board
+      return
     }
 
     // Try every value that fits constraints
@@ -175,14 +235,14 @@ export default function Sudoku() {
     if (getNextEmptySpot(board)[0] !== -1) {
       board[row][col] = 0
     }
-    return board
+    return
   }
 
   function solve () {
-    getSolution(puzzle)
+    getSolution(originalBoard)
   }
 
-  const getNumbers = () => {
+  const initializeDisplay = () => {
     let nums = []
     for (let row = 0; row < puzzle.length; row++) {
       for (let col = 0; col < puzzle[row].length; col++) {
@@ -190,16 +250,36 @@ export default function Sudoku() {
       }
     }
 
+    setNodesList(nums)
+    displayNodes(nums)
+  }
+
+  const displayNodes = (nums) => {
     const numMap = nums.map((num) =>
-      <SmallBox puzzle={puzzle} coords={coordinates} value={num.value} row={num.row} col={num.col} handleClick={handleClick} />
+      <SmallBox constraints={checkVisualizerConstraints} puzzle={puzzle}
+      coords={coordinates.current} value={num.value} row={num.row} col={num.col} handleClick={handleClick}/>
     )
 
     setNumberMap(numMap)
   }
 
+  const adjustNode = (row, col, value) => {
+    const index = (row * 9) + col
+    let nodes = nodesList
+    nodes[index].value = value
+    puzzle[row][col] = value
+    setNodesList(nodes)
+    displayNodes(nodes)
+    return puzzle
+  }
+
+  useEffect(() => {
+    // populate boxes
+    initializeDisplay()
+  }, [])
+
   useEffect(() => {
     if (solving) {
-      let copy = originalBoard
       if (boardState === statesArray.length) {
         setSolving(!solving)
         return
@@ -207,15 +287,11 @@ export default function Sudoku() {
       let row = statesArray[boardState][0]
       let col = statesArray[boardState][1]
       let value = statesArray[boardState][2]
-      copy[row][col] = value
-      originalBoard = copy
-      setCoordinates([row, col])
-      sleep(5)
+      coordinates.current = [row, col]
+      setPuzzle(adjustNode(row, col, value))
+      sleep(60)
       setBoardState(boardState + 1)
     }
-
-    // populate boxes
-    getNumbers()
   }, [coordinates, boardState, solving])
 
   return (
@@ -228,21 +304,30 @@ export default function Sudoku() {
   )
 }
 
-const SmallBox = (props) => {
-  // console.log(props.coords[0], props.coords[1], props.row, props.col)
+const SmallBox = ({constraints, puzzle, coords, value, row, col, handleClick}) => {
   const classes = useStyles()
+  const red = '#FFAAAA'
+  const white = '#FFFFFF'
+  const blue = '#AADDFF'
+  const green = '#BBFFCC'
   function getColor() {
-    const row = props.coords[0]
-    const col = props.coords[1]
-    if (props.row === row && props.col === col) {
-      return '#FF0000'
+    if (coords[0] !== -1) {
+      if (!constraints(puzzle, row, col, value)) {
+        return red
+      }
+    }
+    if (value === 0) {
+      return green
+    }
+    if (row === coords[0] && col === coords[1]) {
+      return blue
     } else {
-      return "#FFFFFF"
+      return white
     }
   }
   return(
-    <div className={classes.smallBox} style={{background: `${getColor()}`}} onClick={() => {props.handleClick(props.row, props.col)}}>
-      {props.puzzle[props.row][props.col]}
+    <div className={classes.smallBox} style={{background: `${getColor()}`}} onClick={() => {handleClick(row, col)}}>
+      {value}
     </div>
   )
 }
